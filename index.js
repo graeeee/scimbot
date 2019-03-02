c009b0d42ec880d9bb5a17e7cb4075d99edd3f42
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-01 20:45:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
   
-  let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
   
   if(!prefixes[message.guild.id]){
     prefixes[message.guild.id] = {
