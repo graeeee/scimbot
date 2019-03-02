f913cb91475c4cadbcf960984eda3e21c65a5c1f
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-01 20:30:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,4 +29,18 @@ client.on("message", async message => {
   if(message.channel.type === "dm") return;
   
   let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+  
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix;
+    };
+  }
+  
+  let prefix = prefixes[message.guild.id].prefixes;
+  console.log(prefix)
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0]
+  let args = messageArray.slice(1);
+      }
+  });
 client.login(process.env.BOT_TOKEN);
