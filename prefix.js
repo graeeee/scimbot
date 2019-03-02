ef29030dc99fc70bec20ea2e2c4da3eaf8dd26da
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-01 20:42:54 -0500

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -6,7 +6,23 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("You have no permission");
   if(!args[0] || args[0 == "help"]) return message.channel.send("**Usage:** !prefix <your prefix>");
 
-  let prefixes = JSON.parse(
+  let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+  
+  prefix[message.guild.id] = {
+    prefix: args[0]
+  };
+  
+  fs.writeFile("./prefix.json", JSON.stringify(prefix), (err) => {
+    if (err) console.log(err)
+  });
+  
+  let pEmbed = new Discord.RichEmbed()
+  .setColor("FFFFFF")
+  .setTitle("Prefix Changed"
+  .setDescription(`Prefix set to **${args[0]}**`);
+            
+            message.channel.send(pEmbed);
+  
 }
   
 module.exports.help = {
