006af464b3f2152efa23d06af0c1b4df0f0d3e14
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-01 21:09:26 -0500

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 exports.run = async (bot, message, args, func) => {
 
-  if (!message.member.hasPermission("ADMINISTRATOR')) return message.channel.send('You need to be a administrator.');
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('You need to be a administrator.');
   if (!args.join(" ")) return message.channel.send("Please enter a argument");
   
   db.updateText(`guildPrefix_${message.guild.id}`, args.join().trim()).then(i => {
