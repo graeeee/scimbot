74bf5869d252672a8185fb199fc2d80ea5f54528
graeeee <46288884+graeeee@users.noreply.github.com>
2019-03-01 22:11:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const botconfig =require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client({diableEveryone: true});
 const fs = require("fs");
-const db = require("quick.db");
 
 
 client.on("ready", async () => {
@@ -25,20 +24,4 @@ client.on("message", async message => {
                 message.channel.send(botembed);
             }
 });
-bot.on('message', (message) => {
-    if (message.channel.type === "dm" || message.author.bot || message.author === bot.user) return; // Checks if we're on DMs, or the Author is a Bot, or the Author is our Bot, stop.
-    var args = message.content.split(' ').slice(1); // We need this later
-    var command = message.content.split(' ')[0].replace(guildConf[message.guild.id].prefix, ''); // Replaces the Current Prefix with this
-
-    if (command === "prefix") {
-	guildConf[message.guild.id].prefix = args[0];
-	if (!guildConf[message.guild.id].prefix) {
-		guildConf[message.guild.id].prefix = config.prefix; // If you didn't specify a Prefix, set the Prefix to the Default Prefix
-	}
-     fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-     	if (err) console.log(err)
-	})
-  }
-});
-
 client.login(process.env.BOT_TOKEN);
