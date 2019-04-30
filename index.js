5635582d3b91bcbfc9648a18aa7ef406d7ad1da3
graeeee <46288884+graeeee@users.noreply.github.com>
2019-04-30 19:01:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,34 +10,21 @@ client.on("ready", async () => {
   client.user.setActivity("HCUnions discord!", {type: "WATCHING"});
 
 });
-exports.run = function(client, message) {
-
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();      
-
-if (command === 'move') {
-  const mem = message.mentions.members.first()
-  const vc = args.join(" ")
-  const chan = client.channels.find("name", vc)
-    mem.setVoiceChannel(chan)
-      .then(() => console.log(`Moved ${mem.displayName} to ${chan}`))
-  .catch(console.error);
-  }
-}
 client.on("message", async message => {
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-    let info = (`${prefix}botinfo`)
+    let info = (`${prefix}info`)
                
                 if(cmd === info){
-      let botpfp = bot.user.displayAvatarURL;
+      let botpfp = client.user.displayAvatarURL;
       let infoembed = new Discord.RichEmbed()
       .setThumbnail(botpfp)
-      .setDescription("**cmonBruh Information**")
-      .setColor("#000000")
-      .addField("Server Count", client.guilds.size);
+      .setDescription("**HCUnions Information**")
+      .setColor("#008080")
+      .addField("**Server IP:**", "us.hcunions.com")
+      .addField("Discord Member Count", client.guilds.size);
                 }
 });
 client.login(process.env.BOT_TOKEN);
