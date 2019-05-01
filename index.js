a681a661cec66727b46912efaaf5074ab1a7acc5
graeeee <46288884+graeeee@users.noreply.github.com>
2019-04-30 20:57:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -43,4 +43,16 @@ client.on("message", async message => {
       message.channel.send(storeembed);
                 }
 });
+client.on("guildMemberAdd") member => {
+  let joinchannel = member.guild.channels.find('name', 'new-members');
+  let memberpfp = member.user.avatarURL
+        let joinembed = new Discord.RichEmbed()
+      .setColor("#008080")
+      .setThumbnail(memberpfp)
+      .setDescription("New User")
+      .addField("New User:", `<@${member}>`)
+      .addField("Member Count:", `${member.guild.memberCount}`)
+      .setTimestamp();
+  message.channel.send(joinembed);
+});
 client.login(process.env.BOT_TOKEN);
