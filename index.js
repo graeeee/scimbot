50aef72abf18b947d359da331e693c47dbd65482
graeeee <46288884+graeeee@users.noreply.github.com>
2019-05-01 21:16:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 client.on("ready", async () => {
-  console.log(`${client.user.username} is online!`);
+  console.log(`npm i request`);
 
   client.user.setActivity("hcunions.org | !info", {type: "PLAYING"});
 
