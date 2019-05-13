b6d6acecabf13ffb98269e3a12e293f51d86d493
graeeee <46288884+graeeee@users.noreply.github.com>
2019-05-13 17:24:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("message", message => {
         .setColor('#1247B5');
         message.channel.send(everyone);
         message.channel.send(embed);
-        await message.delete(everyone);
+        message.delete(everyone);
     }
 });
 client.login(process.env.BOT_TOKEN);
