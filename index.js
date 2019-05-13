acf1ce6abc9610133141134f1b29d57ff47c50ee
graeeee <46288884+graeeee@users.noreply.github.com>
2019-05-13 18:42:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("message", message => {
         const embed = new Discord.RichEmbed()
         .setTitle("Server Announcement")
         .setDescription(ann)
-        .setFooter(message.user.username, message.user.displayAvatarURL)
+        .setTimestamp(message.createdAt);
         .setColor('#1247B5');
         message.channel.send("@everyone");
         message.channel.send(embed)
