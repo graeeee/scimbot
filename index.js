303f4d2ebff5da37621bd8f2429b9e1286106076
graeeee <46288884+graeeee@users.noreply.github.com>
2019-11-15 21:04:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,42 +9,8 @@ client.on("ready", async () => {
   client.user.setActivity("the Official Krunker Krosshair Discord!", {type: "WATCHING"});
 
 });
-client.on("message", async message => {
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    let info = (`${prefix}info`)
-               
-                if(cmd === info){
-      let botpfp = client.user.displayAvatarURL;
-      let infoembed = new Discord.RichEmbed()
-      .setThumbnail(botpfp)
-      .setDescription("**Server Information**")
-      .setColor("#f04abc")
-      .addField ("Website", "https://alpha5v5.org/home")
-      .addField("Store", "https://alpha5v5.org/store")
-      .addField("Server IPs", "https://alpha5v5.org/servers")
-      .addField("Discord Member Count", `${message.guild.memberCount}`);
-      message.channel.send(infoembed);
-                }
-});
-client.on("message", async message => {
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    let store = (`${prefix}store`)
-               
-                if(cmd === store){
-      let storeembed = new Discord.RichEmbed()
-      .setColor("#f04abc")
-      .addField("Store", "http://alpha5v5.org/store");
-      message.channel.send(storeembed);
-                }
-});
 client.on('guildMemberAdd', member => {
-let joinChannel = member.guild.channels.find('name', 'new-members');
+let joinChannel = member.guild.channels.find('name', 'welcome');
 
   let joinEmbed = new Discord.RichEmbed()
   .setAuthor("New User Joined") 
