b0e02dd2fde7c788cea60c093a8b1c5738408d94
graeeee <46288884+graeeee@users.noreply.github.com>
2019-11-17 12:43:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,53 @@ let joinChannel = member.guild.channels.find('name', 'welcome');
   .setColor('#f04abc')
   joinChannel.send(joinEmbed);
 })
+client.on("message", async message =>
+{
+    if(message.author.bot)
+        return;
+    if(message.channel.type == "dm")
+        return;
+       
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+       
+    if(cmd === `${prefix}mute`)
+    {
+       
+        let tempUser = message.mentions.members.first();
+        let tempTime = args[1];
+        let tempRole = message.guild.roles.find(role => role.name === "Muted");
+       
+        if(message.member.roles.find(role => role.name === "Founders") ||
+            message.member.roles.find(role => role.name === "Moderators") ||
+        {
+            if(!tempUser)
+                return message.channel.send('Invalid Usage, **!mute <@User#1234> <time>**');
+           
+            if(!tempTime)
+                return message.channel.send('Invalid Usage, **!mute <@User#1234> <time>**');
+
+            if(!tempReason)
+            {
+                tempReason = "N/A";
+            }
+           
+            await(tempUser.addRole(tempRole.id).catch(console.error));
+           
+            message.channel.send(`<@${tempUser.id}> has been muted for ${tempTime}.`)
+           
+            setTimeout(function()
+            {
+                tempUser.removeRole(tempRole.id);
+            }, ms(tempTime));
+        }
+        else
+        {
+            message.channel.send("No permission.");
+        }
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
